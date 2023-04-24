80de89b3bab11d09f51f864d94e467505d71404d
LJPileggi <88879501+LJPileggi@users.noreply.github.com>
2023-04-24 12:43:35 +0200

    upload commented.

--- preprocess_and_upload/upload.py
+++ preprocess_and_upload/upload.py
@@ -5,6 +5,21 @@ from rich.progress import track
 from rich.console import Console
 
 def write_log(tables_done, curr_row):
+    """
+    Write log file in case of a failure. Log file will have
+    the following template:
+
+    ### LOG FILE ###
+    Completed tables:
+    ###,###,###
+    Number of committed rows:
+    ###
+
+    params:
+        - tables_done: list containing names of the successfully
+          uploaded tables;
+        - curr_row: last row committed in currently uploading file.
+    """
     tables = ""
     for table in tables_done:
         tables = f"{tables}{table},"
@@ -13,6 +28,11 @@ def write_log(tables_done, curr_row):
         log.write(f"Number of committed rows:\n{curr_row}")
 
 def read_log():
+    """
+    Searches for an existing log file, and takes the checkpoint information
+    (tables_done and curr_row). If no log file is found, returns empty list
+    and 0 as current row.
+    """
     try:
         with open("log.txt", 'r') as log:
             rows = log.readlines()
@@ -23,12 +43,18 @@ def read_log():
     return tables_done, curr_row
 
 def delete_log():
+    """
+    Deletes log file once the whole upload is completed.
+    """
     try:
         os.remove("log.txt")
     except FileNotFoundError:
         return
 
 def open_conn():
+    """
+    Defines connection string and returns connection and cursor objects.
+    """
     server = "lds.di.unipi.it"  # lds.di.unipi.it
     database = "pileggi_mura_DB"
     username = "pileggi_mura"
@@ -47,7 +73,10 @@ def open_conn():
     cn = pyodbc.connect(connectionString)
     return cn, cn.cursor()
 
-def close_conn(cn, cursor):#remember to close it always
+def close_conn(cn, cursor):
+    """
+    Close connection and cursor objects.
+    """
     cursor.close()
     cn.close()
 
@@ -60,6 +89,8 @@ def load_tables():
         "time" : os.path.join("..", data_folder, "time.csv"),
         "fact" : os.path.join("..", data_folder, "fact.csv")
     }
+
+    #to account for different field types
     tables_header = {
         "vendor" : {
             "vendor_code" : "int",
@@ -98,56 +129,62 @@ def load_tables():
 
     cn, cursor = open_conn()
 
+    #reads log file if existing
     tables_done, curr_row = read_log()
     for table_name, path in tables_path.items():
+        #if table already present in done tables, do nothing while iterating
         if table_name not in tables_done:
             print(f"Uploading table [{table_name}]...")
             with open(path, 'r') as table_file:
                 reader = csv.DictReader(table_file)
+                #counter variable for committing
                 commit = 0
+                #counter variable for log file
                 lines_done = 0
                 for row in track(reader):
+                    #skip already committed lines
                     if lines_done >= curr_row:
-                        #line to populate to db
                         to_send = ""
                         for row_key, row_value in row.items():
+                            #fix string format
                             if tables_header[table_name][row_key] == "str":
-                                #check if there's an apostrophe in the string and "escape" it
+                                #find apostrophes in string and remove them
                                 position = row_value.find("'")
                                 if position != -1:
                                     row_value = f'{row_value[:position]}{row_value[position+1:]}'
                                 to_send = f"{to_send},'{row_value}'"
                             else:
                                 to_send = f"{to_send},{row_value}"
-                            #to_send = f"{to_send},{row_value}"   
                         try:
 
-                            #table name is the name of our tables (in dict above)
-                            query = f"INSERT INTO [pileggi_mura].[{table_name}] VALUES ({to_send[1:]});" #SQL
+                            query = f"INSERT INTO [pileggi_mura].[{table_name}] VALUES ({to_send[1:]});"
                             cursor.execute(query)
-                        # commit once every 100 rows/queries
                         except Exception as e:
-                            #if except stoppati
-                            if e.args[0] != "23000":#primary key error: don't upload row
+                            #"23000" is primary key error: rather than stopping whole upload,
+                            #simply skip row; otherwise break connection and write log
+                            if e.args[0] != "23000":
                                 print(e)
                                 print(f"Failed upload upon query:\n{query}")
                                 close_conn(cn, cursor)
                                 write_log(tables_done, lines_done)
                                 return
                         commit += 1
+                        #commit every 100 lines and reset counter
                         if commit == 100:
                             cn.commit()
                             lines_done += commit
                             commit = 0
                     else:
                         lines_done += 1
-            # if there are "leftover" rows, commit them too.
+                #commit remaining rows
                 if commit > 0:
                     cn.commit()
             print(f"Upload of table {table_name} performed succesfully.")
+            #record completed table for log file
             tables_done.append(table_name)
+            #reset counter
             lines_done = 0
-    # close connection
+    #close connection
     close_conn(cn, cursor)
     delete_log()
 
